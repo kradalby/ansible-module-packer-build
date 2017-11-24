d1844425a5aadf1da63b87a3d6cb7d52ddb3245d
Kristoffer Dalby <kradalby@kradalby.no>
2017-11-24 16:06:44 +0100

    fixes syntax error

--- __init__.py
+++ __init__.py
@@ -10,7 +10,7 @@ def main():
         argument_spec=dict(
             packer_path=dict(default="", type='str'),
             template=dict(required=True, type='str'),
-            variable=dict(defaul="", type='str'))
+            variable=dict(defaul="", type='str')),
         supports_check_mode=True,
     )
 
@@ -18,9 +18,8 @@ def main():
         module.params['packer_path'])
 
     if not os.path.isfile(packer_exec):
-        module.fail_json(
-            msg='Could not find packer, is the path correct: {}?'.format(
-                packer_exec))
+        module.fail_json(msg='Could not find packer, is the path correct: {}?'.
+                         format(packer_exec))
 
     command_tokens = [packer_exec]
 
@@ -29,13 +28,15 @@ def main():
     ])
 
     if module.params['variable']:
-        command_tokens.extend(['-var-file={}'.format(module.params['variables'])])
+        command_tokens.extend(
+            ['-var-file={}'.format(module.params['variables'])])
 
     ova_tool_result = module.run_command(command_tokens)
 
     if ova_tool_result[0] != 0:
         module.fail_json(
-            msg='Failed to create virtual machine image, error message from packer is: {}'.
+            msg=
+            'Failed to create virtual machine image, error message from packer is: {}'.
             format(ova_tool_result[1]))
 
     module.exit_json(changed=True, ova_tool_result=ova_tool_result)
