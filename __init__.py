4910ba8914133df888bc532295769229db62e0ce
Kristoffer Dalby <kradalby@kradalby.no>
2017-11-24 16:33:52 +0100

    makes packer execute in template dir

--- __init__.py
+++ __init__.py
@@ -9,8 +9,9 @@ def main():
     module = AnsibleModule(
         argument_spec=dict(
             packer_path=dict(default="", type='str'),
+            template_dir=dict(required=True, type='str'),
             template=dict(required=True, type='str'),
-            variable=dict(defaul="", type='str')),
+            variable=dict(default="", type='str')),
         supports_check_mode=True,
     )
 
@@ -20,6 +21,13 @@ def main():
     if not os.path.isfile(packer_exec):
         module.fail_json(msg='Could not find packer, is the path correct: {}?'.
                          format(packer_exec))
+
+    template_file = os.path.join(module.params['template_dir'],
+                                 module.params['template'])
+    if not os.path.isfile(template_file):
+        module.fail_json(
+            msg='Could not find template, is the path correct: {}?'.format(
+                template_file))
 
     command_tokens = [packer_exec]
 
@@ -31,9 +39,10 @@ def main():
         command_tokens.extend(
             ['-var-file={}'.format(module.params['variables'])])
 
-    command_tokens.extend([module.params['template']])
+    command_tokens.extend([template_file])
 
-    ova_tool_result = module.run_command(command_tokens)
+    ova_tool_result = module.run_command(
+        cwd=module.params['template_dir'], command_tokens)
 
     if ova_tool_result[0] != 0:
         module.fail_json(
